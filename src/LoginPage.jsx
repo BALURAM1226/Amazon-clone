69eaec63aced4cbb0e35b986abf91bf3f6e3e89a
Kumavat Baluram <72209634+BALURAM1226@users.noreply.github.com>
2021-05-20 12:09:20 +0530

    Update LoginPage.jsx

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,24 +1,35 @@
 import React,{ useState} from 'react';
 import './LoginPage.css';
-import {Link} from 'react-router-dom';
+import {Link , useHistory } from 'react-router-dom';
 import {auth} from './firebase';
 export default function LoginPage(){
  
- const [email, setEmail] = useState('');
- const [password,setPassword] = useState('');
-  
-  const signIn = e =>{
-    e.preventDefault();
-  }
-  const register = e => {
-    e.preventDefault();
-    auth.createUserWithEmailAndPassword(email,password).then((auth) => {
-        alert("register successfully");
-    }).catch((err) =>{
-      alert(err);
-    });
+ const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  }
+    const signIn = e => {
+        e.preventDefault();
+
+        auth.signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
+    }
+
+    const register = e => {
+        e.preventDefault();
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // it successfully created a new user with email and password
+                if (auth) {
+                    history.push('/')
+                }
+            })
+            .catch(error => alert(error.message))
+    }
 
 
   return(
