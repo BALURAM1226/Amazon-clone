f5c6b1bee3cdbcf8d3670d829bf69cee0b8cb415
Kumavat Baluram <72209634+BALURAM1226@users.noreply.github.com>
2021-05-20 20:09:00 +0530

    Create Images.jsx

---
+++ src/Images.jsx
@@ -0,0 +1,11 @@
+const Images = {
+
+"https://i.ibb.co/SQ8nZMk/Desktop-Teaser-Header.jpg",
+"https://i.ibb.co/JKn3SRk/HK-CB440726831-SL1280-FMjpg.jpg",
+"https://i.ibb.co/2WxXZpM/PC-rec-pantry-SL1280-FMpng.png",
+"https://i.ibb.co/gJXYX9Y/downloadfile-1.jpg"
+
+
+}
+
+export default Images;
