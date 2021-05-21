6930c4484ff679ab84ca0290de946f726f9f2f01
Kumavat Baluram <72209634+BALURAM1226@users.noreply.github.com>
2021-05-21 09:39:01 +0530

    Update Images.jsx

--- src/Images.jsx
+++ src/Images.jsx
@@ -4,5 +4,5 @@ const Images = [
 "https://i.ibb.co/JKn3SRk/HK-CB440726831-SL1280-FMjpg.jpg",
 "https://i.ibb.co/2WxXZpM/PC-rec-pantry-SL1280-FMpng.png",
 "https://i.ibb.co/gJXYX9Y/downloadfile-1.jpg"
-] 
+];
 export default Images;
