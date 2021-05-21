3ddc389f269d8e9711f512ef256af7493c145b3c
Kumavat Baluram <72209634+BALURAM1226@users.noreply.github.com>
2021-05-21 09:48:07 +0530

    Delete Images.jsx

--- src/Images.jsx
+++
@@ -1,8 +0,0 @@
-const Images = [
-"https://i.ibb.co/mySD1jZ/freedom-sale-teaser-page-PC-unrec-08-CB441232371-SL1280-FMjpg.jpg", 
-"https://i.ibb.co/SQ8nZMk/Desktop-Teaser-Header.jpg",
-"https://i.ibb.co/JKn3SRk/HK-CB440726831-SL1280-FMjpg.jpg",
-"https://i.ibb.co/2WxXZpM/PC-rec-pantry-SL1280-FMpng.png",
-"https://i.ibb.co/gJXYX9Y/downloadfile-1.jpg"
-];
-export default Images;
