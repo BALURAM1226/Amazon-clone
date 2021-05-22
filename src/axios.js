59acdaff9e86da6ccd55ef7237f341a2826cab64
Kumavat Baluram <72209634+BALURAM1226@users.noreply.github.com>
2021-05-22 09:00:03 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   // THE API (cloud function) URL
   baseURL: 'https://amzon-clone.herokuapp.com'
-    // "http://localhost:5001/challenge-4b2b2/us-central1/api",
+    
 });
 
 export default instance;
